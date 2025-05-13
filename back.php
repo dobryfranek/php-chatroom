0e2bcf34ea0452f3fffa7c754625986cc12b8ebc
dobryfranek <dobrowolski.franciszek@gmail.com>
2025-05-13 23:27:55 +0200

    niedokończony i niedziałający slop

--- back.php
+++ back.php
@@ -1,5 +1,16 @@
 <?php
+session_start();
 
+$login = $_POST
 
+define("MESSAGES_FILE_PATH", "messages.txt");
+
+if (!file_exists(MESSAGES_FILE_PATH)) {
+    touch(MESSAGES_FILE_PATH);
+}
+
+$messages_file = file(MESSAGES_FILE_PATH);
+
+if
 
 ?>

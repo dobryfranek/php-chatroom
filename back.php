37ad39afd0520a307676616c050b0eeb08e141ab
dobryfranek <dobrowolski.franciszek@gmail.com>
2025-05-14 19:32:16 +0200

    kursor działa (ledwo)

--- back.php
+++ back.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
 
-$login = $_POST
+//login todo here
+
+if (!isset($_SESSION["login"])) {
+    $_SESSION["login"] = "anonymous";
+}
 
 define("MESSAGES_FILE_PATH", "messages.txt");
 
@@ -11,6 +15,20 @@ if (!file_exists(MESSAGES_FILE_PATH)) {
 
 $messages_file = file(MESSAGES_FILE_PATH);
 
-if
+function echo_messages() {
+    echo nl2br(implode("", array_slice($messages_file, -32)));
+    return 0
+}
+
+if (!isset($_POST["message"])) {
+    echo_messages();
+    exit(0);
+}
+
+$message = $_SESSION["login"] . ": " . $_POST["message"];
+
+fwrite($message . "\n")
+
+echo_messages();
 
 ?>

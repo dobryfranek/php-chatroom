4b9d6e3101b09360c671dca84eb23b6a7fe49fac
dobryfranek <dobrowolski.franciszek@gmail.com>
2025-05-22 21:21:08 +0200

    f

--- back.php
+++ back.php
@@ -2,13 +2,28 @@
 
 session_start();
 
+define("MESSAGES_FILE_PATH", "messages.txt");
+define("UPLOAD_DIR", "pliki/");
+define("MESSAGE_RETURN_LIMIT", 32);
+define("LOGIN_LENGTH_LIMIT", 64);
+
+if (!file_exists(MESSAGES_FILE_PATH)) {
+    touch(MESSAGES_FILE_PATH);
+}
+if (!is_dir(UPLOAD_DIR)) {
+    mkdir(UPLOAD_DIR, 0755, true);
+}
+
+if (isset($_FILES["file"])) {
+    move_uploaded_file($_FILES["file"]["tmp_name"], UPLOAD_DIR . $_FILES["file"]["name"]);
+}
 
 if (isset($_POST["set_user"])) {
     $demanded_login = $_POST["set_user"];
     if (str_contains($demanded_login, "|")) {
         $_SESSION["login"] = "konformista";
     } else {
-        $_SESSION["login"] = substr($demanded_login, 0, 64); //limit username length
+        $_SESSION["login"] = substr($demanded_login, 0, LOGIN_LENGTH_LIMIT);
     }
 }
 
@@ -21,17 +36,16 @@ if (isset($_POST["get_user"])) {
     die();
 }
 
-define("MESSAGES_FILE_PATH", "messages.txt");
-
-if (!file_exists(MESSAGES_FILE_PATH)) {
-    touch(MESSAGES_FILE_PATH);
+if (isset($_POST["get_file_list"])) {
+    echo json_encode(array_diff(scandir(UPLOAD_DIR), array("..", ".")));
+    die();
 }
 
 $messages_file_array = file(MESSAGES_FILE_PATH);
 
 function echo_messages() {
     global $messages_file_array;
-    echo implode("", array_slice($messages_file_array, -32));
+    echo implode("", array_slice($messages_file_array, -1 * MESSAGE_RETURN_LIMIT));
 }
 
 if (isset($_POST["message"])) {
